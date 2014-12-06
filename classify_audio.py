b09ec1f26fc6c259d20be79c1a7d619cc7bfdcf2
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-12-06 01:18:11 -0600

    first draft of audio classifier

--- classify_audio.py
+++ classify_audio.py
@@ -2,12 +2,13 @@
 
 from __future__ import division
 
-from subprocess import check_output
+from subprocess import check_call
 from contextlib import contextmanager
 from os import path
 import tempfile
 import shutil
 
+import click
 from funcy import pluck, partial, compose
 from numpy import hstack, ones
 from scipy.io.wavfile import read as wav_read
@@ -40,23 +41,6 @@ def train(data_and_labels, cls):
     return classifier.fit(X, dense_labels)
 
 
-def experiment(training_wavs, test_wavs, load_bucket, is_voice_cls, which_cls):
-    get_data = partial(map, load_bucket)
-    training_data = get_data(training_wavs)
-    test_data = get_data(test_wavs)
-    is_voice = train(zip(training_data, [0, 1, 1]), is_voice_cls)
-    which_speaker = train(zip(training_data[1:], [1, 2]), which_cls)
-
-    activity_data, activity_labels = dense_data_and_labels(zip(test_data,
-                                                               [0, 1, 1]))
-    score1 = is_voice.score(activity_data, activity_labels)
-
-    whois_data, whois_labels = dense_data_and_labels(zip(test_data[1:],
-                                                         [1, 2]))
-    score2 = which_speaker.score(whois_data, whois_labels)
-    return score1, score2
-
-
 def train_classifiers(training_wavs, test_wavs, load_bucket, is_voice_cls, which_cls):
     get_data = partial(map, load_bucket)
     training_data = get_data(training_wavs)
@@ -73,7 +57,7 @@ def train_classifiers(training_wavs, test_wavs, load_bucket, is_voice_cls, which
 def to_wav(mp4_path):
     with TemporaryDirectory() as d:
         wav_path = path.join(d, "out.wav")
-        check_output(["ffmpeg", "-i", mp4_path, wav_path])
+        check_call(["ffmpeg", "-v", "0", "-i", mp4_path, wav_path])
         return wav_read(wav_path)
 
 
@@ -94,18 +78,15 @@ def batch(arr, fps, winlen=WINLEN):
     return array(map(median, array_split(arr, len(arr)//n)))
 
 
-# TODO: take mp4 as argument
-# TODO: return mat file
-def main():
-    print(experiment(["silence1.wav", "marcell1.wav", "faraz1.wav"],
-                     ["silence2.wav", "marcell2.wav", "faraz2.wav"],
-                     wav_to_features, LinearSVC, GaussianNB))
-    
+@click.command()
+@click.argument('input_mp4')
+@click.argument('output_mat')
+def main(input_mp4, output_mat):
     classify = train_classifiers(["silence1.wav", "marcell1.wav", "faraz1.wav"],
                                  ["silence2.wav", "marcell2.wav", "faraz2.wav"],
                                  wav_to_features, LinearSVC, GaussianNB)
     
-    savemat("out.mat", {"x": classify(mp4_to_features("videos/1_stationary_single.mp4").T)})
+    savemat(output_mat, {"x": classify(mp4_to_features(input_mp4).T)})
 
 if __name__ == '__main__':
     main()
